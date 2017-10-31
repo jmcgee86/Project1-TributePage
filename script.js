3605f1a040408eea84cbcf4973ecc8fd6b5b4031
Jim McGee <jmcgee86@gmail.com>
2017-10-31 00:42:47 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("sarac").addEventListener("mouseover", function(){
+  document.getElementById("artinfo").innerHTML = "<h1> Saracenia</h1> <p>Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi.<a href='https://en.wikipedia.org/wiki/Sarracenia' target='_blank'> More information</a></p>";
+});
+
+document.getElementById("terrapin").addEventListener("mouseover", function(){
+  document.getElementById("artinfo").innerHTML = "<h1> Diamondback Terrapin</h1> <p class=>Etiam sit amet sagittis est, at dapibus erat. Aliquam erat volutpat. Curabitur vitae mauris ut mi bibendum porta. Mauris faucibus tincidunt lectus nec efficitur. Integer sed elit hendrerit, tempor sem a, sodales nibh. Nam a lorem urna. Aliquam erat volutpat. Etiam vitae felis mattis, ultrices eros ac, blandit diam. Aliquam pretium dolor a justo condimentum tempus. Proin hendrerit venenatis tortor vitae tincidunt. Pellentesque interdum sollicitudin venenatis. Vestibulum maximus purus leo, at condimentum augue lacinia at. Nullam congue ut tellus eu feugiat. Nulla eget nisi imperdiet, condimentum turpis eu, efficitur ipsum. Nam sit amet vulputate ex.<a href= '#'>More information.</a></p>";
+});
+
+document.getElementById("parakareka").addEventListener("mouseover", function(){
+  document.getElementById("artinfo").innerHTML = "<h1> Parakareka</h1> <p>Aliquam erat volutpat. Etiam vitae felis mattis, ultrices eros ac, blandit diam. Aliquam pretium dolor a justo condimentum tempus. Proin hendrerit venenatis tortor vitae tincidunt. Pellentesque interdum sollicitudin venenatis. Vestibulum maximus purus leo, at condimentum augue lacinia at. Nullam congue ut tellus eu feugiat. Nulla eget nisi imperdiet, condimentum turpis eu, efficitur ipsum. Nam sit amet vulputate ex. <a href= '#'>More information.</a></p>";
+});
+
+document.getElementById("gooty").addEventListener("mouseover", function(){
+  document.getElementById("artinfo").innerHTML = "<h1> Gooty</h1> <p> Mauris faucibus tincidunt lectus nec efficitur. Integer sed elit hendrerit, tempor sem a, sodales nibh. Nam a lorem urna. Vestibulum maximus purus leo, at condimentum augue lacinia at. Nullam congue ut tellus eu feugiat. Nulla eget nisi imperdiet, condimentum turpis eu, efficitur ipsum. Nam sit amet vulputate ex. <a href= '#'>More information.</a></p>";
+});
